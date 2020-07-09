dd812ce905a80dfa0c4f00ca183120fd14ba48d4
shalinie <shalushalinie22@gmail.com>
2020-07-09 13:50:15 +0530

    vvvvvvvv

--- index.js
+++ index.js
@@ -1,41 +1,29 @@
-
-
 const express = require("express");
-
-const bodyparser=require("body-parser")
-
 const app = express();
+const bodyparser = require("body-parser");
+app.use(bodyparser.json());
 const port = process.env.PORT || 4000;
-app.use(bodyparser.json())
-var student_details=[{
-    "id":1,
-    "name":"sha",
-    "email":"shalushalinie22@gmail.com",
-    "staff_id":10
-}]
+let student_details = [];
+let staff_details = [];
+app.listen(port, () => {
+  console.log("hello",port);});
+  app.post("/student", (req, res) => {
+    student_details.push(req.body);
+    console.log("allllllllllll")
+    res.json({"msg":"no issue"})
+  });
+  app.post("/staff", (req, res) => {
+    staff_details.push(req.body);
+    console.log(staff_details);
+    res.json({
+      message: "staff-details are created",});
+  });
+  app.get("/staffget", (req, res) => {
+    res.json(staff_details);
+  });
 
-var staff_details=[{
-    "id":10,
-    "name":"shalu",
-    "email":"shalu@gmail.com"
-}]
-app.listen(4000,()=>{
-    console.log("hello",port)
-});
-
-app.post("/staffdetails",(req,res)=>{
-    staff_details.push(...req.body)
-})
-app.post("/student",(req,res)=>{
-    student_details.push(...req.body)
-    console.log("okkkk")
-})
-
-app.get("/user",(req,res)=>{
-    res.send(student_details)
-   // console.log(student_details)
-})
-app.get("/ssttaa",(req,res)=>{
-    res.send(staff_details)
-   // console.log(student_details)
-})
+  app.get("/studentget", (req, res) => {
+    student_details.push(req.body);
+    console.log("students data is created");
+    res.json(student_details);
+  });
